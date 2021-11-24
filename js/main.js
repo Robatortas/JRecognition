06a0f000fb2a475c12d22d06901f4f784e7575ec
Robatortas <robatortas1@gmail.com>
2021-11-23 23:44:26 -0800

    Canvas draw

--- js/main.js
+++ js/main.js
@@ -1,11 +1,31 @@
 const video = document.getElementById('video');
 
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri("/library"),
+    faceapi.nets.faceLandmark68Net.loadFromUri("/library"),
+    faceapi.nets.faceRecognitionNet.loadFromUri("/library"),
+    faceapi.nets.faceExpressionNet.loadFromUri("/library")
+]).then(startVideo)
+
 function startVideo(){
     navigator.getUserMedia(
         { video: {} },
         stream => video.srcObject = stream,
-        err => console.error(err)
+        error => console.error(error)
         )
 }
 
 startVideo();
+
+video.addEventListener('play', () => {
+    const canvas = faceapi.createCanvasFromMedia(video);
+    document.body.append(canvas);
+    const displaySize = { width: video.width, height: video.height }
+    console.log("Started Video");
+    setInterval(async () => {
+        const detect = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+        console.log(detect);
+        const reziseDetect = faceapi.resizeResults(detect, displaySize);
+        faceapi.draw.drawDetections(canvas, resizeDetect);
+    }, 100)
+})
