546fcaf253c47a379d80e18f30d6eba143b9acfe
Robatortas <robatortas1@gmail.com>
2022-12-20 22:45:40 -0800

    Test

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 const video = document.getElementById('video');
-const diego = document.getElementById("diego");
 
 Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri("/library"),
-    faceapi.nets.faceLandmark68Net.loadFromUri("/library"),
-    faceapi.nets.faceRecognitionNet.loadFromUri("/library"),
-    faceapi.nets.faceExpressionNet.loadFromUri("/library"),
-    faceapi.nets.ageGenderNet.loadFromUri('/library')
+  faceapi.nets.tinyFaceDetector.loadFromUri("/library"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/library"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("/library"),
+  faceapi.nets.faceExpressionNet.loadFromUri("/library"),
+  faceapi.nets.ageGenderNet.loadFromUri('/library')
 ]).then(startVideo)
 
 function startVideo(){
